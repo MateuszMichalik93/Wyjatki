abeef9dbacd6273c90bfbb0ccbd281b64887c044
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-05-19 21:00:04 +0200

    dodanie klas

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,4 +1,36 @@
 public class Product {
 
+   private String productName;
+   private double price;
+  private   Category category;
 
+    public Product(String productName, double price, Category category) {
+        this.productName = productName;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
